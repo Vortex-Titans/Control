fea35f95bf18c707a42692ae0e10402c22ec51ac
yahyaThe8Hokage <yahyaamr2382004@gmail.com>
2025-03-20 05:59:49 +0200

    Update joystick axis and mapping and add reconection logic
    -changed up_down axis from 2 to 3 for better control
    -updated thruster mapping
      -1500 to 1200 if moving up
      -1500 to 1800 if moving down
    -Added reconnection logic to handle disconection and reconection

--- joystickControl_py.py
+++ joystickControl_py.py
@@ -6,14 +6,27 @@ class JoystickController:
     def __init__(self):
         pygame.init()
         pygame.joystick.init()
-        self.joystick = pygame.joystick.Joystick(0)
-        self.joystick.init()
+        self.connect_joystick()
         print(f"Initialized Joystick: {self.joystick.get_name()}")
         self.prev_1 = self.prev_2 = 0
-        self.pump_1 = self.pump_2 = self.pump_3 = self.pump_4 = self.pump_5 = self.pump_6 = 0
-        self.pwm_1 = self.pwm_2 = self.pwm_3 = self.pwm_4 = self.pwm_5 = self.pwm_6 = 0
+        self.pump_1 = self.pump_3 = self.pump_4 = self.pump_6 = 0
+        self.pwm_1 = self.pwm_3 = self.pwm_4 = self.pwm_6 = 0
         self.thruster_1 = self.thruster_2 = 1500
         self.gripper_1, self.gripper_2 = False, False
+
+
+    def connect_joystick(self):
+
+        while pygame.joystick.get_count() == 0:
+            #WAITS FOR THE CONNECTION 
+            print("waiting for connection")
+            pygame.time.wait(1000)
+        self.joystick = pygame.joystick.Joystick(0)
+        self.joystick.init()    
+
+    def is_joystick_connected(self):
+        #checks for connection
+        return self.joystick is not None and pygame.joystick.get_count()
 
     def map_value(x, in_min, in_max, out_min, out_max):
         return (x - in_min) / (in_max - in_min) * (out_max - out_min) + out_min
@@ -22,27 +35,21 @@ class JoystickController:
         pygame.event.pump()
         os.system("cls" if os.name == "nt" else "clear")
 
-        x_axis = self.joystick.get_axis(0)
+        
         y_axis = self.joystick.get_axis(1)
 
-        if abs(y_axis) > abs(x_axis):
-            pwm = int(abs(y_axis) * 255)
-            self.pwm_1 = self.pwm_2 = self.pwm_3 = self.pwm_4 = self.pwm_5 = self.pwm_6 = pwm
-            self.pump_1 = self.pump_2 = self.pump_3 = self.pump_4 = self.pump_5 = self.pump_6 = 1 if y_axis > 0 else 0
-        elif abs(x_axis) > abs(y_axis):
-            pwm = int(abs(x_axis) * 255)
-            self.pwm_1 = self.pwm_3 = self.pwm_4 = self.pwm_6 = pwm
-            self.pwm_2 = self.pwm_5 = 0
-            if x_axis > 0:
-                self.pump_1, self.pump_3, self.pump_4, self.pump_6 = 0, 0, 1, 1
-            else:
-                self.pump_1, self.pump_3, self.pump_4, self.pump_6 = 1, 1, 0, 0
+        
+        pwm = int(abs(y_axis) * 255)
+        self.pwm_1 = self.pwm_3 = self.pwm_4 = self.pwm_6 = pwm
+        self.pump_1 = self.pump_3 = self.pump_4 = self.pump_6 = 1 if y_axis > 0 else 0
+        
+    
 
-        up_down = self.joystick.get_axis(2)
+        up_down = self.joystick.get_axis(3)
         if up_down < 0:
-            self.thruster_1, self.thruster_2 = 2000, 2000
+            self.thruster_1 = self.thruster_2 = int(JoystickController.map_value(abs(up_down),0,1,1500,1200))
         elif up_down > 0:
-            self.thruster_1, self.thruster_2 = 1000, 1000
+            self.thruster_1 =  self.thruster_2 = int(JoystickController.map_value(up_down,0,1,1500,1800))
 
         rotate_right = self.joystick.get_axis(4)
         if rotate_right != -1:
@@ -58,9 +65,9 @@ class JoystickController:
 
         if self.prev_1 == 0 and self.joystick.get_button(0) == 1:
             self.gripper_1 = not self.gripper_1
-        self.prev_1 = self.joystick.get_button(0
+        self.prev_1 = self.joystick.get_button(0)
                                                 
-                                                )
+                                                
         if self.prev_2 == 0 and  self.joystick.get_button(1):
             self.gripper_2 = not self.gripper_2
         self.prev_2 = self.joystick.get_button(1)
@@ -70,26 +77,27 @@ class JoystickController:
         return (
             f"{self.thruster_1:04d}{self.thruster_2:04d}"
             f"{self.pump_1}{self.pwm_1:03d}"
-            f"{self.pump_2}{self.pwm_2:03d}"
+            
             f"{self.pump_3}{self.pwm_3:03d}"
             f"{self.pump_4}{self.pwm_4:03d}"
-            f"{self.pump_5}{self.pwm_5:03d}"
+          
             f"{self.pump_6}{self.pwm_6:03d}"
-            f"{int(self.gripper_1)}{int(self.gripper_2)}\n"
+            f"{int(self.gripper_1)}{int(self.gripper_2)}"
         )
 
 
     def run(self):
         while True:
-            self.update_controls()
-            msg = self.get_message()
-            print(msg)  # Print the message to the console
-            print("Message Length:", len(msg))
+            if not self.is_joystick_connected():
+                self.connect_joystick()
+            else:    
+                self.update_controls()
+                msg = self.get_message()
+                print(msg)  # Print the message to the console
+                print("Message Length:", len(msg))
             
 
 if __name__ == "__main__":
     joystick_controller = JoystickController()
     joystick_controller.run()
-
-
 
